ac39947d9424a8abe2fdc87dcca0f8ad807743ea
Canol Okoth <canolowana6@gmail.com>
2026-03-11 00:32:46 +0300

    new updates

--- crop-frontend/src/api/api.js
+++ crop-frontend/src/api/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Create an axios instance
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
-  //baseURL: "http://199.231.191.165:8000/api/",
+  //baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://199.231.191.165:8000/api/",
   headers: {
     "Content-Type": "application/json",
   },
